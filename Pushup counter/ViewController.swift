2c7f6af57bc60696d9f3cf941e29fdc669ff53fe
Kirill Ki <kirillkalapov@gmail.com>
2022-01-19 19:05:54 +0100

    Initial Commit

---
+++ Pushup counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pushup counter
+//
+//  Created by  Mr.Ki on 19.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
