73b8f32d15d182cee83c294759192cdf192deb6d
Kirill Ki <kirillkalapov@gmail.com>
2022-01-19 22:55:33 +0100

    storyboard complete

---
+++ Pushup counter/CounterViewController.swift
@@ -0,0 +1,31 @@
+//
+//  CounterViewController.swift
+//  Pushup counter
+//
+//  Created by  Mr.Ki on 19.01.2022.
+//
+
+import UIKit
+
+class CounterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func doneBarButton(_ sender: UIBarButtonItem) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Pushup counter/ViewController.swift
+++ Pushup counter/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var pickerOutlet: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBOutlet weak var pickerPressed: UIPickerView!
+    
+    @IBAction func barButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func startButtonPressed(_ sender: UIButton) {
+        
+        
+    }
+    
 
 }
 
