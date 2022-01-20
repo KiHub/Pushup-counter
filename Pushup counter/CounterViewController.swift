7adb74d39341658dfc2c11602a94e80e8c3408d1
Kirill Ki <kirillkalapov@gmail.com>
2022-01-20 01:49:09 +0100

    proximity sensor activation added

--- Pushup counter/CounterViewController.swift
+++ Pushup counter/CounterViewController.swift
@@ -27,21 +27,21 @@ class CounterViewController: UIViewController {
         let circularPath = UIBezierPath(arcCenter: center, radius: 100,
                                         startAngle: -CGFloat.pi / 2, endAngle: 2 * CGFloat.pi, clockwise: true)
         trackLayer.path = circularPath.cgPath
-        trackLayer.strokeColor = #colorLiteral(red: 0.2509803922, green: 0.231372549, blue: 0.5843137255, alpha: 1)
-        trackLayer.lineWidth = 10
+        trackLayer.strokeColor = #colorLiteral(red: 0.9960784314, green: 0.7607843137, blue: 0.3764705882, alpha: 0.8470588235)
+        trackLayer.lineWidth = 20
         trackLayer.fillColor = UIColor.clear.cgColor
         trackLayer.lineCap = .round
         view.layer.addSublayer(trackLayer)
        
         shapeLayer.path = circularPath.cgPath
-        shapeLayer.strokeColor = #colorLiteral(red: 0.6520697474, green: 0.7128807902, blue: 0.7406589985, alpha: 0.8470588235)
-        shapeLayer.lineWidth = 20
+        shapeLayer.strokeColor = #colorLiteral(red: 0.2509803922, green: 0.231372549, blue: 0.5843137255, alpha: 1)
+        shapeLayer.lineWidth = 10
         shapeLayer.strokeEnd = 0
         shapeLayer.fillColor = UIColor.clear.cgColor
         shapeLayer.lineCap = .round
         view.layer.addSublayer(shapeLayer)
         
-        
+        activateProximitySensor()
        
     }
     
@@ -77,5 +77,21 @@ class CounterViewController: UIViewController {
      
         
     }
+    
+    func proximityChanged(notification: NSNotification) {
+      if let device = notification.object as? UIDevice {
+        print ("\(device) detected!")
+        label.text = String(labelNumber - 1 )
+      }
+    }
+            
+    func activateProximitySensor() {
+      let device = UIDevice.current
+        device.isProximityMonitoringEnabled = true
+        if device.isProximityMonitoringEnabled {
+            NotificationCenter.default.addObserver(self, selector: Selector(("proximityChanged:")), name: NSNotification.Name(rawValue: "UIDeviceProximityStateDidChangeNotification"), object: device)
+        }
+      }
+    
 
 }
