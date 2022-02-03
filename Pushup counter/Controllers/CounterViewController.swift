fa94a0eba480e86627ea8e6f2ea14b31590f958c
Kirill Ki <kirillkalapov@gmail.com>
2022-02-03 18:25:58 +0100

    Update first counter

--- Pushup counter/Controllers/CounterViewController.swift
+++ Pushup counter/Controllers/CounterViewController.swift
@@ -13,15 +13,24 @@ class CounterViewController: UIViewController {
     
     let doneSound = SimpleSound(named: "win")
     var labelNumber = 55
+    var sets = 3
+    var currentSet = 0
+    
     var currentNumber = 0
     let date = Date()
     let formatter = DateFormatter()
     var days: [Day] = []
     
+    var timer: Timer!
+    var timeRemaining = 30
+    var setThree = 30
+    
+    
     @IBOutlet weak var cupImage: UIImageView!
     @IBOutlet weak var circularProgressView: KDCircularProgress!
     @IBOutlet weak var label: UILabel!
     
+    @IBOutlet weak var thumbImage: UIImageView!
     
     //    override func viewWillAppear(_ animated: Bool) {
     //        super.viewWillAppear(animated)
@@ -35,8 +44,7 @@ class CounterViewController: UIViewController {
     //    }
     
     override func viewWillAppear(_ animated: Bool) {
-        UIDevice.current.isProximityMonitoringEnabled = true
-        UIApplication.shared.isIdleTimerDisabled = true
+        
     }
     
     override func viewDidLoad() {
@@ -52,8 +60,14 @@ class CounterViewController: UIViewController {
         circularProgressView.angle = 0
         activateProximitySensor()
         formatter.dateFormat = "MM-dd-YYYY"
+        thumbImage.isHidden = true
+        
+        UIDevice.current.isProximityMonitoringEnabled = true
+        UIApplication.shared.isIdleTimerDisabled = true
+        currentSet = 0
         
     }
+        
     //MARK: - Deactivate proximity sensor
     override func viewWillDisappear(_ animated: Bool) {
         UIDevice.current.isProximityMonitoringEnabled = false
@@ -84,7 +98,7 @@ class CounterViewController: UIViewController {
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         return appDelegate.persistentContainer.viewContext
     }
-    //MARK: - Uodate counter
+    //MARK: - Update counter
     func update() {
         if currentNumber != labelNumber - 1 {
             //  print("labekNumber:\(labelNumber)")
@@ -109,6 +123,147 @@ class CounterViewController: UIViewController {
             saveDayDate(trainingDate: dateString)
         }
     }
+//
+//    func updateNew(currentNumber: Int) {
+//
+//        for set in sets {
+//          //  currentNumber = 0
+//            if currentNumber = labelNumber {
+//                circularProgressView.angle = 360
+//                label.text = ""
+//                if currentSet != sets {
+//                thumbUp()
+//                    UIDevice.current.isProximityMonitoringEnabled = false
+//                    doneSound.play()
+//                }
+//
+//            } else {
+//                <#statements#>
+//            }
+//
+//
+//        }
+//
+//    }
+    
+    
+    func updateWithSets() {
+        
+        if currentSet != sets {
+           // circularProgressView.angle = 0
+            circularProgressView.alpha = 1.0
+            label.text = "0"
+            print("Part3")
+        if currentNumber != labelNumber - 1 {
+            print(currentNumber, ">>>", labelNumber)
+            //  print("labekNumber:\(labelNumber)")
+            circularProgressView.angle += Double(360 / labelNumber)
+            currentNumber += 1
+            label.text = String(currentNumber)
+
+            print("Part1")
+        } else {
+            circularProgressView.angle = 360
+            //  label.text = "Done!"
+            label.text = ""
+            
+            currentSet += 1
+            
+            if currentSet != sets {
+            thumbUp()
+                UIDevice.current.isProximityMonitoringEnabled = false
+                doneSound.play()
+                currentNumber = 0
+               
+                label.text = "0"
+
+            } else {
+                circularProgressView.alpha = 1.0
+                circularProgressView.angle = 360
+                label.text = ""
+                cupImage.isHidden = false
+                UIDevice.current.isProximityMonitoringEnabled = false
+                doneSound.play()
+                
+                let formatter = DateFormatter()
+                formatter.dateFormat = "dd-MM-yyyy"
+                let dateString = formatter.string(from: date)
+                
+                saveDayDate(trainingDate: dateString)
+                print("Part4")
+                
+            }
+            
+            print("Part2")
+        
+        }
+        }
+//        else {
+//            circularProgressView.alpha = 1.0
+//            circularProgressView.angle = 360
+//
+//            label.text = ""
+//            cupImage.isHidden = false
+//            UIDevice.current.isProximityMonitoringEnabled = false
+//            doneSound.play()
+//
+//            let formatter = DateFormatter()
+//            formatter.dateFormat = "dd-MM-yyyy"
+//            let dateString = formatter.string(from: date)
+//
+//            saveDayDate(trainingDate: dateString)
+//            print("Part4")
+//        }
+    }
+    
+    func rest() {
+        UIDevice.current.isProximityMonitoringEnabled = false
+        circularProgressView.animate(fromAngle: 360, toAngle: 0, duration: TimeInterval(setThree), completion: nil)
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(stepOne), userInfo: nil, repeats: true)
+        
+        
+    }
+    
+    @objc func stepOne() {
+        if timeRemaining > 0 {
+            timeRemaining -= 1
+        } else {
+          //  cupImage.isHidden = false
+          //  doneSound.play()
+            timer.invalidate()
+        //    timeRemaining = setThree
+          //  exerciseLabel.text = ""
+            //MARK: - ToDo Save data and set check mark
+        }
+        label.text = "\(timeRemaining)"
+    }
+    
+    func thumbUp() {
+        
+   //     fetchedTrainingDaysArray[row].done.setValue(true, forKey: "done")
+        
+    //    saveData()
+        
+        thumbImage.isHidden = false
+        thumbImage.alpha = 1.0
+        circularProgressView.alpha = 1.0
+        label.text = ""
+      //  counterLabel.isHidden = true
+
+        UIView.animate(withDuration: 0.5, delay: 1.0, options: [], animations: {
+
+                    self.thumbImage.alpha = 0.0
+                    self.circularProgressView.alpha = 0.0
+           // self.circularProgressView.angle = 0
+
+                }) { (finished: Bool) in
+                    
+                    UIDevice.current.isProximityMonitoringEnabled = true
+                    self.thumbImage.isHidden = true
+                //    self.counterLabel.isHidden = false
+                }
+    }
+    
     //MARK: - Proximity sensor
     func activateProximitySensor() {
         let device = UIDevice.current
@@ -121,8 +276,10 @@ class CounterViewController: UIViewController {
     @objc func proximityChanged(notification: NSNotification) {
         if let device = notification.object as? UIDevice {
             if device.proximityState {
-                update()
+               // update()
+                updateWithSets()
             }
         }
     }
 }
+
--- Pushup counter/Controllers/ViewController.swift
+++ Pushup counter/Controllers/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController{
     @IBOutlet weak var picker: UIPickerView!
     
     var selected = 25
+    var selectedSet = 3
+    var selectedSetsArray = ["x1", "x2", "x3"]
+    
+    
     
     override func viewWillAppear(_ animated: Bool) {
         UIApplication.shared.isIdleTimerDisabled = false
@@ -21,7 +25,8 @@ class ViewController: UIViewController{
         super.viewDidLoad()
         self.picker.delegate = self
         self.picker.dataSource = self
-        picker.selectRow(24, inComponent: 0, animated: true)
+        picker.selectRow(25, inComponent: 0, animated: true)
+        picker.selectRow(2, inComponent: 1, animated: true)
         
     }
     
@@ -47,6 +52,7 @@ class ViewController: UIViewController{
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let counterVC = segue.destination as? CounterViewController else {return}
         counterVC.labelNumber = selected
+        counterVC.sets = selectedSet
     }
     
 }
@@ -54,16 +60,52 @@ class ViewController: UIViewController{
 extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource  {
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
+        return 2
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return 360
+        
+        switch component {
+        case 0:
+            return 360
+        case 1:
+            return 20
+      //      return 20
+        default:
+              return 0
+        }
+        
+       // return 360
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        
+        switch component {
+        case 0:
+            return "\(row)"
+        case 1:
+            return "\(row)"
+        default:
+              return ""
+        }
+        
     }
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        selected = row + 1
-        print(selected)
+        
+        switch component {
+        case 0:
+            selected = row
+            print("selected: \(selected)")
+        case 1:
+            selectedSet = row + 1
+            print("selectedSet: \(selectedSet)")
+        default:
+              break
+        }
+        
+      //  selected = row + 1
+      //  print(selected)
     }
     
     func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
@@ -73,11 +115,31 @@ extension ViewController: UIPickerViewDelegate, UIPickerViewDataSource  {
     func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
         let  view = UIView(frame: CGRect(x: 0, y: 0, width: 250, height: 100))
         let label = UILabel(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height))
-        label.text = String(row+1)
-        label.textColor = #colorLiteral(red: 0.2509803922, green: 0.231372549, blue: 0.5843137255, alpha: 1)
-        label.textAlignment = .center
-        label.font = UIFont.systemFont(ofSize: 35, weight: UIFont.Weight.medium)
-        view.addSubview(label)
-        return view
+        
+        switch component {
+        case 0:
+            label.textColor = #colorLiteral(red: 0.2509803922, green: 0.231372549, blue: 0.5843137255, alpha: 1)
+            label.textAlignment = .center
+            label.font = UIFont.systemFont(ofSize: 35, weight: UIFont.Weight.medium)
+            view.addSubview(label)
+            label.text = String(row)
+            return view
+        case 1:
+            label.textColor = #colorLiteral(red: 0.2509803922, green: 0.231372549, blue: 0.5843137255, alpha: 1)
+            label.textAlignment = .center
+            label.font = UIFont.systemFont(ofSize: 35, weight: UIFont.Weight.medium)
+            view.addSubview(label)
+            label.text = "x\(row + 1)"
+            return view
+        default:
+              return view
+        }
+        
+      //  label.text = String(row)
+//        label.textColor = #colorLiteral(red: 0.2509803922, green: 0.231372549, blue: 0.5843137255, alpha: 1)
+//        label.textAlignment = .center
+//        label.font = UIFont.systemFont(ofSize: 35, weight: UIFont.Weight.medium)
+//        view.addSubview(label)
+  //     return view
     }
 }
